fcaed758b45c90812b0664c770921abea2507730
William Grundler <wrgrundler@gmail.com>
2020-06-15 23:05:43 -0700

    fixed models

--- models/exercise.js
+++ models/exercise.js
@@ -46,8 +46,8 @@ const workoutSchema = new Schema({
 )
 
 workoutSchema.virtual("totalDuration").get(function () {
-    return this.exercise.reduce((total, exercise) => {
-        return total + exercise.duration
+    return this.exercises.reduce((total, exercise) => {
+        return total + exercise.duration;
     }, 0)
 })
 
